9bb79373c223288e499a814d7f09a7a744efd292
ANSHyeon <hyeonan1220@gmail.com>
2024-05-28 16:43:30 +0900

    feat: FeedViewModel, Paging으로 검색 결과 받아오기 구현

---
+++ app/src/main/java/com/anshyeon/imagesearchapp/ui/screen/feedScreen/FeedViewModel.kt
@@ -0,0 +1,77 @@
+package com.anshyeon.imagesearchapp.ui.screen.feedScreen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.anshyeon.imagesearchapp.data.model.UnsplashImage
+import com.anshyeon.imagesearchapp.data.repository.UnsplashImageRepository
+import com.anshyeon.imagesearchapp.utilities.Constants
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class FeedViewModel @Inject constructor(
+    private val unsplashImageRepository: UnsplashImageRepository,
+) : ViewModel() {
+
+    private val _searchQuery = MutableStateFlow("")
+    val searchQuery: StateFlow<String> = _searchQuery
+
+    private val _feedUiState: MutableStateFlow<FeedUiState> = MutableStateFlow(FeedUiState.Start)
+    val feedUiState: StateFlow<FeedUiState> = _feedUiState
+
+    private val _searchResults: MutableStateFlow<PagingData<UnsplashImage>> =
+        MutableStateFlow(PagingData.empty())
+    val searchResults: StateFlow<PagingData<UnsplashImage>> = _searchResults
+
+    @OptIn(FlowPreview::class)
+    val debouncedSearchQuery: Flow<String> = searchQuery
+        .debounce(Constants.SEARCH_DEBOUNCE_TIME)
+        .filter { it.isNotEmpty() }
+        .distinctUntilChanged()
+
+    init {
+        viewModelScope.launch {
+            getSearchResults()
+                .collect { pagingData ->
+                    _searchResults.emit(pagingData)
+                    setFeedUiState(FeedUiState.Success)
+                }
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private fun getSearchResults(): Flow<PagingData<UnsplashImage>> {
+        return debouncedSearchQuery
+            .flatMapLatest { queryString ->
+                unsplashImageRepository.searchImages(queryString) {
+                }.cachedIn(viewModelScope)
+            }
+    }
+
+    fun updateQuery(newQuery: String) {
+        _searchQuery.value = newQuery
+    }
+
+    private fun setFeedUiState(uiState: FeedUiState) {
+        viewModelScope.launch {
+            _feedUiState.emit(uiState)
+        }
+    }
+}
+
+sealed interface FeedUiState {
+    data object Start : FeedUiState
+    data object Success : FeedUiState
+}
